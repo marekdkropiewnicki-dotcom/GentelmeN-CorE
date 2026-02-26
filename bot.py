11f345a9283ce3a9f5eed825dfac3a83e8e1f55e
Marek Daniel Gauza-Kropiewnicki <marek.d.kropiewnicki@gmail.com>
2026-02-26 06:06:59 +0000

    poprawka

--- bot.py
+++ bot.py
@@ -1,2 +1,9 @@
- 	import telebot
-	bot.infinity_polling()
+import telebot
+
+bot = telebot.TeleBot("8616433290:AAHj-PNbbp2gmgzzd7PMD-E-Yj48wQNAJ64")
+
+@bot.message_handler(func=lambda m: True)
+def echo(m):
+    bot.reply_to(m, "GentelmeN@CorE online!")
+
+bot.infinity_polling()
